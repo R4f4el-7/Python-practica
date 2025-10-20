d2a60caf6267da79ca2a53bfbc6fd344585129ed
Rafael Cosquillo <cosquillo00@gmail.com>
2025-10-20 13:50:57 +0200

    08_R

--- bucle_08_cuentaAtras.py
+++ bucle_08_cuentaAtras.py
@@ -1,8 +1,11 @@
 '''Ejercicio 2: Cuenta atrás
 Escribir un programa que pida al usuario un número entero positivo y muestre por pantalla
 la cuenta atrás desde ese número hasta cero separados por comas'''
-num = int(input('Numero: '))
+try:
+    num = int(input('Numero: '))
 
-while(num >= 0):
-    print(f'{num}')
-    num -= 1
+    while(num >= 0):
+        print(f'{num}')
+        num -= 1
+except ValueError:
+    print('valor invalido')
