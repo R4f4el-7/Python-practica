15eaa03710e59e932bfc4f3f060f2bd2ed2cda15
Rafael Cosquillo <cosquillo00@gmail.com>
2026-02-12 09:01:52 +0100

    c 9 d

--- ficheros/csv_09_buscar.py
+++
@@ -1,8 +0,0 @@
-'''Enunciado
-Escribe un programa que lea un archivo CSV llamado clientes.csv y
- busque un cliente específico por su nombre, mostrando todos los detalles de dicho cliente.'''
-import csv
-with open("estudiantes.csv","r") as f:
-    for linea in f:
-        
-        print(linea.strip())
